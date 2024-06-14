78e6ba4718e45691f61d354b6cb18b02a17dc79b
Kharisma Nur Devyanti <hollandbakery22990@gmail.com>
2024-06-14 14:10:38 +0700

    melakukan templating

---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,26 @@
+<!-- Warning Section Starts -->
+<!-- Older IE warning message -->
+<!--[if lt IE 10]>
+<![endif]-->
+<!-- Warning Section Ends -->
+<!-- Required Jquery -->
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/jquery-ui/jquery-ui.min.js "></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/popper.js/popper.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/bootstrap/js/bootstrap.min.js "></script>
+<!-- waves js -->
+<script src="assets/pages/waves/js/waves.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/jquery-slimscroll/jquery.slimscroll.js "></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/SmoothScroll.js"></script>
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js "></script>
+<!-- i18next.min.js -->
+<script type="text/javascript" src="<?= base_url('assets/'); ?>bower_components/i18next/js/i18next.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
+<script type="text/javascript" src="<?= base_url('assets/'); ?>assets/js/common-pages.js"></script>
+</body>
+
+</html>
